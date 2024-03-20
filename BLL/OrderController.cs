34367dfb1c45e19183043a3298d14a082db614ef
MacWielgosz <92044905+MacWielgosz@users.noreply.github.com>
2024-03-20 11:27:30 +0100

    Zrobiono na zajeciach

---
+++ BLL/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    [ApiController]
+    [Route("[controller]")]
+    internal class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public void PostOrder()
+        {
+
+        }
+        [HttpGet]
+        public IEnumerable<Order> GetOrder()
+        {
+
+        }
+
+        [HttpGet("{idUser}")]
+        public IEnumerable<Order> GetOrder(int idUser)
+        {
+
+        }
+    }
+}
---
+++ Models/BasketPosition.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("BasketPositions")]
+    public class BasketPosition : IEntityTypeConfiguration<BasketPosition>
+    {
+        [Key]
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        [ForeignKey(nameof(ProductID))]
+        public Product? Product { get; set; }
+        public int UserID { get; set; }
+        [ForeignKey(nameof(UserID))]
+        public User? User { get; set; }
+        
+        public int Amount { get; set; }
+
+        public void Configure(EntityTypeBuilder<BasketPosition> builder)
+        {
+            builder
+                .HasOne(x => x.User)
+                .WithMany(x => x.BasketPositions)
+                .OnDelete(DeleteBehavior.SetNull);
+
+
+            builder
+                .HasOne(x => x.Product)
+                .WithMany(x => x.BasketPositions)
+                .OnDelete(DeleteBehavior.SetNull);
+
+        }
+    }
+}
